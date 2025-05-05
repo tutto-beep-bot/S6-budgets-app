c06d7462a03a7c3cf96b58d90340065291e343d8
Davi Lessa <davilessa.br@gmail.com>
2025-05-05 13:13:24 +0200

    logic of the reactive form in typescript created.

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -10,5 +10,29 @@ import { CommonModule } from '@angular/common';
 })
 
 export class WelcomeComponent {
+  private formBuilder = Inject(FormBuilder);
 
+  form: FormGroup = this.formBuilder.group({
+    seo: [false],
+    ads: [false],
+    web: [false]
+  });
+
+  prices = {
+    seo: 300, 
+    ads: 400,
+    web: 500,
+  }
+
+  total = signal(0);
+
+  constructor() {
+    this.form.valueChanges.subscribe(values => {
+      const sum = Object.entries(values)
+        .filter(([_, checked]) => checked)
+        .reduce((acc, [key]) => acc + this.prices[key as keyof typeof this.prices], 0)
+
+      this.total.set(sum)
+    })
+  }
 }
