72dc190089af0c246c0b6ea36faf7981f1acd36f
Davi Lessa <davilessa.br@gmail.com>
2025-05-09 21:52:09 +0200

    start-page component was implemented and is now working.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StartPageComponent } from './start-page/start-page.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', component: StartPageComponent},
+    {path: 'welcome', component: WelcomeComponent}
+];
